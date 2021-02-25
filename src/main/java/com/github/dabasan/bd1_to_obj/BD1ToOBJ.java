ccf36e990c92da72a510d7af529d554cb5dc8115
Dabasan <43123127+Dabasan@users.noreply.github.com>
2021-02-25 21:46:56 +0900

    Reformat code

--- src/main/java/com/github/dabasan/bd1_to_obj/BD1ToOBJ.java
+++ src/main/java/com/github/dabasan/bd1_to_obj/BD1ToOBJ.java
@@ -1,60 +1,56 @@
 package com.github.dabasan.bd1_to_obj;
 
+import com.github.dabasan.jxm.bd1.BD1Manipulator;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.github.dabasan.jxm.bd1.BD1Manipulator;
-
 /**
  * BD1をOBJに変換します。
- * 
- * @author Daba
  *
+ * @author Daba
  */
 public class BD1ToOBJ {
-	/**
-	 * 変換処理
-	 * 
-	 * @param filepathBD1
-	 *            BD1ファイルのファイルパス
-	 * @param filepathOBJ
-	 *            OBJファイルのファイルパス
-	 * @param invertZ
-	 *            Z軸を反転するかどうか
-	 * @param flipV
-	 *            テクスチャのV座標を反転するかどうか
-	 * @return エラー: -1 成功: 0
-	 */
-	public int convert(String filepathBD1, String filepathOBJ, boolean invertZ, boolean flipV) {
-		BD1Manipulator manipulator;
-		try {
-			manipulator = new BD1Manipulator(filepathBD1);
-		} catch (IOException e) {
-			System.err.println("Java-side error: Failed to open file.");
-			e.printStackTrace();
+    /**
+     * 変換処理
+     *
+     * @param filepathBD1 BD1ファイルのファイルパス
+     * @param filepathOBJ OBJファイルのファイルパス
+     * @param invertZ     Z軸を反転するかどうか
+     * @param flipV       テクスチャのV座標を反転するかどうか
+     * @return エラー: -1 成功: 0
+     */
+    public int convert(String filepathBD1, String filepathOBJ, boolean invertZ, boolean flipV) {
+        BD1Manipulator manipulator;
+        try {
+            manipulator = new BD1Manipulator(filepathBD1);
+        } catch (IOException e) {
+            System.err.println("Java-side error: Failed to open file.");
+            e.printStackTrace();
 
-			return -1;
-		}
+            return -1;
+        }
 
-		if (invertZ) {
-			manipulator.invertZ();
-		}
+        if (invertZ) {
+            manipulator.invertZ();
+        }
 
-		String baseFilepathOBJ = this.getBaseFilepath(filepathOBJ);
-		String filepathMTL = baseFilepathOBJ + ".mtl";
+        String baseFilepathOBJ = this.getBaseFilepath(filepathOBJ);
+        String filepathMTL = baseFilepathOBJ + ".mtl";
 
-		Path pathMTL = Paths.get(filepathMTL);
-		String filenameMTL = pathMTL.getFileName().toString();
+        Path pathMTL = Paths.get(filepathMTL);
+        String filenameMTL = pathMTL.getFileName().toString();
 
-		return manipulator.saveAsOBJ(filepathOBJ, filepathMTL, filenameMTL, flipV);
-	}
-	private String getBaseFilepath(String filepath) {
-		int lastDotPos = filepath.lastIndexOf('.');
-		if (lastDotPos == -1) {
-			return filepath;
-		}
+        return manipulator.saveAsOBJ(filepathOBJ, filepathMTL, filenameMTL, flipV);
+    }
 
-		return filepath.substring(0, lastDotPos);
-	}
+    private String getBaseFilepath(String filepath) {
+        int lastDotPos = filepath.lastIndexOf('.');
+        if (lastDotPos == -1) {
+            return filepath;
+        }
+
+        return filepath.substring(0, lastDotPos);
+    }
 }
--- src/main/java/com/github/dabasan/bd1_to_obj/BD1ToOBJMain.java
+++ src/main/java/com/github/dabasan/bd1_to_obj/BD1ToOBJMain.java
@@ -1,90 +1,85 @@
 package com.github.dabasan.bd1_to_obj;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
 
 public class BD1ToOBJMain {
-	private static String VERSION_STR = "BD1ToOBJ v1.0.0-rc2";
+    private static String VERSION_STR = "BD1ToOBJ v1.0.0-rc2";
 
-	public static void main(String[] args) {
-		// コマンドライン引数を解析するための設定
-		var options = new Options();
+    public static void main(String[] args) {
+        // コマンドライン引数を解析するための設定
+        var options = new Options();
 
-		var optionI = new Option("i", "inputFilepath", true, "Input filepath");
-		var optionO = new Option("o", "outputFilepath", true, "Output filepath");
-		var optionZ = new Option("z", "invertZ", false,
-				"Inverts the model with respect to the Z-axis");
-		var optionF = new Option("f", "flipV", false, "Flips texture V-coordinate");
-		var optionH = new Option("h", "help", false, "Displays help");
-		var optionV = new Option("v", "version", false, "Displays version info");
+        var optionI = new Option("i", "inputFilepath", true, "Input filepath");
+        var optionO = new Option("o", "outputFilepath", true, "Output filepath");
+        var optionZ = new Option("z", "invertZ", false,
+                "Inverts the model with respect to the Z-axis");
+        var optionF = new Option("f", "flipV", false, "Flips texture V-coordinate");
+        var optionH = new Option("h", "help", false, "Displays help");
+        var optionV = new Option("v", "version", false, "Displays version info");
 
-		optionZ.setArgs(0);
-		optionF.setArgs(0);
-		optionH.setArgs(0);
-		optionV.setArgs(0);
+        optionZ.setArgs(0);
+        optionF.setArgs(0);
+        optionH.setArgs(0);
+        optionV.setArgs(0);
 
-		options.addOption(optionI);
-		options.addOption(optionO);
-		options.addOption(optionZ);
-		options.addOption(optionF);
-		options.addOption(optionH);
-		options.addOption(optionV);
+        options.addOption(optionI);
+        options.addOption(optionO);
+        options.addOption(optionZ);
+        options.addOption(optionF);
+        options.addOption(optionH);
+        options.addOption(optionV);
 
-		// コマンドライン引数の解析
-		var parser = new DefaultParser();
-		CommandLine cmd;
-		try {
-			cmd = parser.parse(options, args);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return;
-		}
+        // コマンドライン引数の解析
+        var parser = new DefaultParser();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return;
+        }
 
-		// ヘルプの表示
-		if (cmd.hasOption("h")) {
-			var hf = new HelpFormatter();
-			hf.printHelp("[options]", options);
-			System.out.println("Specify amount of rotation in degree.");
+        // ヘルプの表示
+        if (cmd.hasOption("h")) {
+            var hf = new HelpFormatter();
+            hf.printHelp("[options]", options);
+            System.out.println("Specify amount of rotation in degree.");
 
-			return;
-		}
-		// バージョン情報の表示
-		if (cmd.hasOption("v")) {
-			System.out.println(VERSION_STR);
-			return;
-		}
+            return;
+        }
+        // バージョン情報の表示
+        if (cmd.hasOption("v")) {
+            System.out.println(VERSION_STR);
+            return;
+        }
 
-		String inputFilepath = cmd.getOptionValue("i");
-		String outputFilepath = cmd.getOptionValue("o");
+        String inputFilepath = cmd.getOptionValue("i");
+        String outputFilepath = cmd.getOptionValue("o");
 
-		if (inputFilepath == null) {
-			System.err.println("Java-side error: Input filepath must be specified.");
-			return;
-		}
-		if (outputFilepath == null) {
-			System.err.println("Java-side error: Output filepath must be specified.");
-			return;
-		}
+        if (inputFilepath == null) {
+            System.err.println("Java-side error: Input filepath must be specified.");
+            return;
+        }
+        if (outputFilepath == null) {
+            System.err.println("Java-side error: Output filepath must be specified.");
+            return;
+        }
 
-		boolean invertZ = false;
-		boolean flipV = false;
-		if (cmd.hasOption("z")) {
-			invertZ = true;
-		}
-		if (cmd.hasOption("f")) {
-			flipV = true;
-		}
+        boolean invertZ = false;
+        boolean flipV = false;
+        if (cmd.hasOption("z")) {
+            invertZ = true;
+        }
+        if (cmd.hasOption("f")) {
+            flipV = true;
+        }
 
-		// 変換処理
-		var bd1ToObj = new BD1ToOBJ();
-		int ret = bd1ToObj.convert(inputFilepath, outputFilepath, invertZ, flipV);
-		if (ret == -1) {
-			System.err.println("Java-side error: Failed to convert.");
-			return;
-		}
-	}
+        // 変換処理
+        var bd1ToObj = new BD1ToOBJ();
+        int ret = bd1ToObj.convert(inputFilepath, outputFilepath, invertZ, flipV);
+        if (ret == -1) {
+            System.err.println("Java-side error: Failed to convert.");
+            return;
+        }
+    }
 }
